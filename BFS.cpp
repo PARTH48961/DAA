486e8ff54417f518d1c491127fd4243142e0de0b
PARTH48961 <72405632+PARTH48961@users.noreply.github.com>
2021-06-24 00:47:22 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -9,22 +9,18 @@ struct Vertex {
    bool visited;
 };
 
-//queue variables
-
 int queue[MAX];
 int rear = -1;
 int front = 0;
 int queueItemCount = 0;
 
-//graph variables
 
-//array of vertices
+
 struct Vertex* lstVertices[MAX];
 
-//adjacency matrix
+
 int adjMatrix[MAX][MAX];
 
-//vertex count
 int vertexCount = 0;
 
 //queue functions
@@ -43,9 +39,6 @@ bool isQueueEmpty() {
    return queueItemCount == 0;
 }
 
-//graph functions
-
-//add vertex to the vertex list
 void addVertex(char label) {
    struct Vertex* vertex = (struct Vertex*) malloc(sizeof(struct Vertex));
    vertex->label = label;  
@@ -53,18 +46,16 @@ void addVertex(char label) {
    lstVertices[vertexCount++] = vertex;
 }
 
-//add edge to edge array
 void addEdge(int start,int end) {
    adjMatrix[start][end] = 1;
    adjMatrix[end][start] = 1;
 }
 
-//display the vertex
 void displayVertex(int vertexIndex) {
    printf("%c ",lstVertices[vertexIndex]->label);
 }       
 
-//get the adjacent unvisited vertex
+
 int getAdjUnvisitedVertex(int vertexIndex) {
    int i;
 	
@@ -79,21 +70,19 @@ int getAdjUnvisitedVertex(int vertexIndex) {
 void breadthFirstSearch() {
    int i;
 
-   //mark first node as visited
+   
    lstVertices[0]->visited = true;
 
-   //display the vertex
    displayVertex(0);   
 
-   //insert vertex index in queue
    insert(0);
    int unvisitedVertex;
 
    while(!isQueueEmpty()) {
-      //get the unvisited vertex of vertex which is at front of the queue
+      
       int tempVertex = removeData();   
 
-      //no adjacent vertex found
+      
       while((unvisitedVertex = getAdjUnvisitedVertex(tempVertex)) != -1) {    
          lstVertices[unvisitedVertex]->visited = true;
          displayVertex(unvisitedVertex);
@@ -101,8 +90,7 @@ void breadthFirstSearch() {
       }
 		
    }   
-
-   //queue is empty, search is complete, reset the visited flag        
+      
    for(i = 0;i<vertexCount;i++) {
       lstVertices[i]->visited = false;
    }    
@@ -111,23 +99,23 @@ void breadthFirstSearch() {
 int main() {
    int i, j;
 
-   for(i = 0; i<MAX; i++) // set adjacency 
+   for(i = 0; i<MAX; i++) 
    {
-      for(j = 0; j<MAX; j++) // matrix to 0
+      for(j = 0; j<MAX; j++) 
          adjMatrix[i][j] = 0;
      }
-   addVertex('S');   // 0
-   addVertex('A');   // 1
-   addVertex('B');   // 2
-   addVertex('C');   // 3
-   addVertex('D');   // 4
+   addVertex('S');   
+   addVertex('A');   
+   addVertex('B');   
+   addVertex('C');   
+   addVertex('D');   
  
-   addEdge(0, 1);    // S - A
-   addEdge(0, 2);    // S - B
-   addEdge(0, 3);    // S - C
-   addEdge(1, 4);    // A - D
-   addEdge(2, 4);    // B - D
-   addEdge(3, 4);    // C - D
+   addEdge(0, 1);    
+   addEdge(0, 2);    
+   addEdge(0, 3);    
+   addEdge(1, 4);    
+   addEdge(2, 4);    
+   addEdge(3, 4);    
 	
    printf("\nBreadth First Search: ");
    
