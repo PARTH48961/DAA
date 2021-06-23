e5878f567ed84467a0c8a12dd9059921706dc5f8
PARTH48961 <72405632+PARTH48961@users.noreply.github.com>
2021-06-24 00:51:28 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -9,23 +9,23 @@ struct Vertex {
    bool visited;
 };
 
-//stack variables
+
 
 int stack[MAX]; 
 int top = -1; 
 
-//graph variables
 
-//array of vertices
+
+
 struct Vertex* lstVertices[MAX];
 
-//adjacency matrix
+
 int adjMatrix[MAX][MAX];
 
-//vertex count
+
 int vertexCount = 0;
 
-//stack functions
+
 
 void push(int item) { 
    stack[++top] = item; 
@@ -43,9 +43,6 @@ bool isStackEmpty() {
    return top == -1;
 }
 
-//graph functions
-
-//add vertex to the vertex list
 void addVertex(char label) {
    struct Vertex* vertex = (struct Vertex*) malloc(sizeof(struct Vertex));
    vertex->label = label;  
@@ -53,18 +50,17 @@ void addVertex(char label) {
    lstVertices[vertexCount++] = vertex;
 }
 
-//add edge to edge array
+
 void addEdge(int start,int end) {
    adjMatrix[start][end] = 1;
    adjMatrix[end][start] = 1;
 }
 
-//display the vertex
 void displayVertex(int vertexIndex) {
    printf("%c ",lstVertices[vertexIndex]->label);
 }       
 
-//get the adjacent unvisited vertex
+
 int getAdjUnvisitedVertex(int vertexIndex) {
    int i;
 
@@ -80,20 +76,20 @@ int getAdjUnvisitedVertex(int vertexIndex) {
 void depthFirstSearch() {
    int i;
 
-   //mark first node as visited
+ 
    lstVertices[0]->visited = true;
 
-   //display the vertex
+   
    displayVertex(0);   
 
-   //push vertex index in stack
+   
    push(0);
 
    while(!isStackEmpty()) {
-      //get the unvisited vertex of vertex which is at top of the stack
+     
       int unvisitedVertex = getAdjUnvisitedVertex(peek());
 
-      //no adjacent vertex found
+      
       if(unvisitedVertex == -1) {
          pop();
       } else {
@@ -102,8 +98,7 @@ void depthFirstSearch() {
          push(unvisitedVertex);
       }
    }
-
-   //stack is empty, search is complete, reset the visited flag        
+       
    for(i = 0;i < vertexCount;i++) {
       lstVertices[i]->visited = false;
    }        
@@ -112,24 +107,24 @@ void depthFirstSearch() {
 int main() {
    int i, j;
 
-   for(i = 0; i < MAX; i++)    // set adjacency 
+   for(i = 0; i < MAX; i++)    
    {
-      for(j = 0; j < MAX; j++) // matrix to 0
+      for(j = 0; j < MAX; j++) 
          adjMatrix[i][j] = 0;
    }
 
-   addVertex('S');   // 0
-   addVertex('A');   // 1
-   addVertex('B');   // 2
-   addVertex('C');   // 3
-   addVertex('D');   // 4
+   addVertex('S');   
+   addVertex('A');   
+   addVertex('B');   
+   addVertex('C');   
+   addVertex('D');   
 
-   addEdge(0, 1);    // S - A
-   addEdge(0, 2);    // S - B
-   addEdge(0, 3);    // S - C
-   addEdge(1, 4);    // A - D
-   addEdge(2, 4);    // B - D
-   addEdge(3, 4);    // C - D
+   addEdge(0, 1);    
+   addEdge(0, 2);    
+   addEdge(0, 3);   
+   addEdge(1, 4);    
+   addEdge(2, 4);    
+   addEdge(3, 4);   
 
    printf("Depth First Search: ");
    depthFirstSearch(); 
